1616b611e57b3282be3dc971abf5a5a37ebe6fa0
Paula Santana Galvão <psantgalvao@gmail.com>
2022-01-26 15:45:48 -0300

    Projeto finalizado

---
+++ pecas.js
@@ -0,0 +1,17 @@
+let PesoPeca = 333
+
+if (PesoPeca > 100) {console.log
+    
+("Peso maior que 100g, logo, é possível cadastrar a peça!") } 
+else {console.log("Peso insuficiente, logo, não é possível cadastrar!")}
+
+let NumeroPecas = 3
+
+if(NumeroPecas < 10) {console.log("Ainda há espaço na caixa, então é possível cadastrar a peça!")}
+else {console.log("Não há espaço disponível na caixa!")}
+
+let NomePeca = 'Trio'
+
+console.log("O comprimento do nome da peça é:", NomePeca.length)
+if (NomePeca.length < 3){console.log("Nome de peça muito curto, não é possível cadastrar!")}
+else{console.log("Nome de peça adequado, é possível cadastrar!")}
